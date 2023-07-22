c491b76f57fe3acfac980c3b1cf1e1aefcdabda8
Miski <Miski@Miskis-Air.cust.communityfibre.co.uk>
2023-07-22 13:05:54 +0100

    updating work

---
+++ Javascript/homework-1.js
@@ -0,0 +1,12 @@
+console.log ("hello" + " " + "world")
+
+var firstword = "hello";
+var secondword = "world";
+console.log (firstword)
+console.log (secondword)
+
+var firstname = "Abdiaziz";
+let age = 28;
+const dob = "23/8/1994";
+
+console.log( " Hello my name is", firstname, " I am" , age, " years old. I was born on", dob);
