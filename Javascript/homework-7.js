674dfb5b638046140c31ca10df9bfb213b0e1b7b
Miski <Miski@Miskis-Air.cust.communityfibre.co.uk>
2023-08-13 19:12:31 +0100

    project

--- Javascript/homework-7.js
+++ Javascript/homework-7.js
@@ -1,44 +1,4 @@
-let day = "Saturday"
-
-
-switch(day){
-    case 'Monday':
-    console.log ("7 Hours");
-    break
-    case 'Tueday':
-    console.log ("5 Hours");
-    break
-    case 'Wednesday':
-    console.log ("8 Hours");
-    break
-    case 'Thursday':
-    console.log ("4 Hours");
-    break
-    case 'Friday':
-    console.log ("6 Hours");
-    break
-    case 'Saturday':
-    console.log ("3 Hours");
-    break
-    case 'Sunday':
-    console.log ("9 Hours");   
-    
-}
-
-function findaverage(){
-    var sum=0;
-    var average=0;
-    sum=7+5+8+4+6+3+9;
-    average=sum/7;
-    console.log("The average sleeping hour is " + average);
-
-}
-    function findaverage()
-
-
-
-
-//create a function that converts fahrenheit to kelvin
+  //create a function that converts fahrenheit to kelvin
 // K=((℉-32)/1.8)+273.15
 
 function farenheit(num){
@@ -53,4 +13,24 @@ function Farenheittokelvin(){
 }
 Farenheittokelvin()
 
+
+function findaverage(){
+    var sum=0;
+    var average=0;
+    sum=7+5+8+4+6+3+9;
+    average=sum/7;
+    console.log("The average sleeping hour is " + average);
+
+}
+    findaverage()
+
+       
+    var hoursslept = [7,5,8,4,6,3,9]
+    sum = 42
+    return 
+
+function idealsleephours(num){
+    
+
+}    
 
